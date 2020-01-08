ff2dd7c505f4b5aac830af4b5e4c59da697c34b0
Filipe Gomes <filipe.gomes.ext@tivit.com>
2020-01-08 13:30:34 -0300

    Initialized

---
+++ src/main/java/com/brasilPrev/desafio/BrasilPrev/DesafioBrasilPrevApplication.java
@@ -0,0 +1,13 @@
+package com.brasilPrev.desafio.BrasilPrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBrasilPrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBrasilPrevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brasilPrev/desafio/BrasilPrev/DesafioBrasilPrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasilPrev.desafio.BrasilPrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBrasilPrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
